3a193121e3cd8af7a0382d57e5ddd128277bf36d
Dom Webber <dom.webber@hotmail.com>
2026-02-06 15:24:48 +0000

    Fix Reaction message type and add EventNotificationMessageReaction type

--- src/types/Message/MessageType.ts
+++ src/types/Message/MessageType.ts
@@ -13,8 +13,8 @@ export enum MessageType {
   Image = "image", // for image messages.
   Interactive = "interactive", // for list and reply button messages.
   Location = "location", // for location messages.
-  Reaction = "sticker", // for reaction messages.
-  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
+  Reaction = "reaction", // for reaction messages.
+
   Sticker = "sticker", // for sticker messages.
 
   /** Not supported for Outgoing Messages. */

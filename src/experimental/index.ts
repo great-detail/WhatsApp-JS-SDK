bb6bd4c6dfaf12e545d2ca4a5cba908d973e49fe
Dom Webber <dom.webber@hotmail.com>
2026-09-22 11:42:16 +0100

    Add more schemas

--- src/experimental/index.ts
+++ src/experimental/index.ts
@@ -5,10 +5,15 @@ export * as clientWhatsappBusinessAccounts from "../__generated/business-messagi
 export * as ownedWhatsappBusinessAccounts from "../__generated/business-messaging_whatsapp_reference_business_owned-whatsapp-business-accounts_v25.0-types";
 export * as preVerifiedPhoneNumberSharing from "../__generated/business-messaging_whatsapp_reference_business_whatsapp-business-pre-verified-phone-number-sharing-api_v25.0-types";
 export * as preVerifiedPhoneNumbers from "../__generated/business-messaging_whatsapp_reference_business_whatsapp-business-pre-verified-phone-numbers-api_v25.0-types";
+export * as media from "../__generated/business-messaging_whatsapp_reference_media_media-api_v23.0-types";
+export * as messageHistoryEvents from "../__generated/business-messaging_whatsapp_reference_message-history_whatsapp-business-message-history-events-api_v25.0-types";
+export * as assignedUsersManagement from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_assigned-users-management-api_v25.0-types";
 export * as conversationalAutomation from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_conversational-automation-api_v25.0-types";
+export * as extendedCredits from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_extended-credits-api_v25.0-types";
 export * as messageTemplate from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_message-template-api_v25.0-types";
 export * as phoneNumberManagement from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_phone-number-management-api_v25.0-types";
 export * as subscribedApps from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_subscribed-apps-api_v25.0-types";
+export * as businessAccountActivities from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_whatsapp-business-account-activities-api_v25.0-types";
 export * as whatsappBusinessAccount from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-account_whatsapp-business-account-api_v25.0-types";
 export * as blockUsers from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-phone-number_block-api_v25.0-types";
 export * as message from "../__generated/business-messaging_whatsapp_reference_whatsapp-business-phone-number_message-api_v25.0-types";
